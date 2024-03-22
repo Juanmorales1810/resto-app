ea340e494c5f3c81e6d5f28ab8e49bae9cd6a9bd
Juan Domingo Exequiel Morales <85465719+Juanmorales1810@users.noreply.github.com>
2024-03-22 12:56:21 -0300

    Agregado registro de cuerpo de respuesta en la función POST

--- app/api/notify/route.ts
+++ app/api/notify/route.ts
@@ -8,10 +8,12 @@ export async function POST(req: NextRequest, res: NextResponse) {
     const body = await res
         .json()
         .then((data) => data as { data: { id: string } });
-    const secret = req.headers.get("x-request-id_header");
-    if (secret !== process.env.MERCADO_PAGO_SECRET) {
-        return NextResponse.json({ success: false });
-    }
+    console.log(body);
+
+    // const secret = req.headers.get("x-request-id_header");
+    // if (secret !== process.env.MERCADO_PAGO_SECRET) {
+    //     return NextResponse.json({ success: false });
+    // }
     const payment = await new Payment(mercadopago).get({ id: body.data.id });
 
     const donation = {
